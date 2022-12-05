503975a1772fbfa8e36a528d7f56f7f31967ad1c
javals243 <vchihisa@gmail.com>
2022-12-05 14:16:13 +0200

    initial commits

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import ToggleButton from "./ToggleButton";
+import SliderComponent from "./Slider";
+import Accordion from "./Accordion";
+import HamBurger from "./HamBurger";
+import HamBurgerDropdown from "./HamBurgerDropdown";
+import RadioButton from "./RadioButton";
+import Carousel from "./Carousel";
+import ContextMenu from "./ContextMenu";
+import IconWithToolTips from "./IconWithTooltips";
+import NavBar from "./NavBar";
+
+export {
+  ToggleButton,
+  SliderComponent,
+  Accordion,
+  HamBurger,
+  HamBurgerDropdown,
+  RadioButton,
+  Carousel,
+  ContextMenu,
+  IconWithToolTips,
+  NavBar,
+};
